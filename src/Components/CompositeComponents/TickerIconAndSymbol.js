f9b1bc4b189d58b3d2e7a77e1a2804d0cf344567
Sharan <sharanram1993@gmail.com>
2022-07-08 01:17:38 +0530

    Create Image component

--- src/Components/CompositeComponents/TickerIconAndSymbol.js
+++ src/Components/CompositeComponents/TickerIconAndSymbol.js
@@ -1,12 +1,17 @@
 import classNames from "classnames";
 
+import Image from "../AtomicComponents/Image";
+
 function TickerIconAndSymbol({ icon, symbol, classes = {} }) {
   return (
     <div className={classNames(classes?.iconAndSymbolWrapper)}>
       <div className={classNames(classes?.imageWrapper)}>
-        <img
+        <Image
           src={icon}
-          className={classNames(classes?.image)}
+          classes={{
+            imageWrapper: classes?.imageWrapper,
+            image: classes.image,
+          }}
           onError={(e) => {
             e.target.onerror = null;
             e.target.src = "";
