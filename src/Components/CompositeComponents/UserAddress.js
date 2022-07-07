bd6349edd2fca66623073dde7e7f1fadddfba344
Sharan <sharanram1993@gmail.com>
2022-07-07 23:41:33 +0530

    Create sharable link when copyIcon next to address is clicked

--- src/Components/CompositeComponents/UserAddress.js
+++ src/Components/CompositeComponents/UserAddress.js
@@ -1,9 +1,25 @@
 import { useSelector } from "react-redux";
 import { getUserAddress } from "../../redux/user";
 
+import CopyIcon from "../../assets/images/copyIcon.png";
+
 function UserAddress() {
   const userAddress = useSelector(getUserAddress);
-  return <div className="text-sm text-gray-500">{userAddress}</div>;
+  return (
+    <div className="flex text-sm text-gray-500 items-center">
+      <div>{userAddress}</div>
+      <div
+        className="w-3 ml-2 cursor-pointer"
+        onClick={() =>
+          navigator.clipboard.writeText(
+            `${window.location.origin}/${userAddress}`
+          )
+        }
+      >
+        <img src={CopyIcon} alt="copyIcon" />
+      </div>
+    </div>
+  );
 }
 
 export default UserAddress;
--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,23 @@
+import { useDispatch } from "react-redux";
+import { setUserAddress } from "./redux/user";
+
 import MainContent from "./MainContent";
+import { useEffect } from "react";
 
 const SideBar = () => {
   return <div className="w-1/5 bg-white"></div>;
 };
 
-const Layout = () => {
+const Layout = ({
+  match: {
+    params: { userAddress = "0x5ed3608242a1e9cccf45198d78ac2c7d54cd484f" },
+  },
+}) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setUserAddress(userAddress));
+  }, [dispatch, userAddress]);
+
   return (
     <div className="flex w-full">
       <SideBar />
