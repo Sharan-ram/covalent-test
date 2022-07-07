8b5a5584fde08aa55aaa9009f383faf6ee809ccf
Sharan <sharanram1993@gmail.com>
2022-07-07 13:35:16 +0530

    Move tabs to profile section

--- src/Components/CompositeComponents/Sections/Details.js
+++ src/Components/CompositeComponents/Sections/Details.js
@@ -1,15 +1,13 @@
 import { useSelector } from "react-redux";
 import { getActiveTab } from "../../../redux/tabs";
 
-import Tabs from "../../AtomicComponents/Tabs";
 import Assets from "../Assets";
 import Transactions from "../Transactions";
 
 function DetailSection() {
   const activeTab = useSelector(getActiveTab);
   return (
-    <div className="w-5/6 mx-auto">
-      <Tabs />
+    <div className="w-5/6 mx-auto pt-4">
       {activeTab === "assets" && <Assets />}
       {activeTab === "transactions" && <Transactions />}
     </div>
--- src/MainContent.js
+++ src/MainContent.js
@@ -2,6 +2,8 @@ import SearchSection from "./Components/CompositeComponents/Sections/Search";
 import ProfileSection from "./Components/CompositeComponents/Sections/Profile";
 import DetailSection from "./Components/CompositeComponents/Sections/Details";
 
+import Tabs from "./Components/AtomicComponents/Tabs";
+
 const MainContent = () => {
   return (
     <div className="w-4/5">
@@ -10,6 +12,9 @@ const MainContent = () => {
       </div>
       <div className="bg-[#fafbfc]">
         <ProfileSection />
+        <div className="w-5/6 mx-auto mt-4">
+          <Tabs />
+        </div>
       </div>
       <div className="bg-[#eff3f8] pb-8">
         <DetailSection />
