44917197d06191729aac4f377a27be732b552b9d
Sharan <sharanram1993@gmail.com>
2022-07-08 12:44:12 +0530

    REfactor Tabs component

--- src/Components/AtomicComponents/Tabs.js
+++ src/Components/AtomicComponents/Tabs.js
@@ -3,31 +3,30 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { getActiveTab, toggleActiveTab } from "../../redux/tabs";
 
-function Tabs() {
+function Tabs({
+  tabs = [
+    { name: "Assets", value: "assets" },
+    { name: "Transactions", value: "transactions" },
+  ],
+}) {
   const activeTab = useSelector(getActiveTab);
   const dispatch = useDispatch();
   return (
     <div className="flex justify-between w-1/4">
-      <div
-        onClick={() => dispatch(toggleActiveTab())}
-        className={classnames({
-          "w-1/2 pb-2.5 text-sm text-[#8b93a7] font-semibold flex justify-center cursor-pointer": true,
-          "border-b-4 text-orange-500/100 border-b-orange-500/100":
-            activeTab === "assets",
-        })}
-      >
-        Assets
-      </div>
-      <div
-        onClick={() => dispatch(toggleActiveTab())}
-        className={classnames({
-          "w-1/2 pb-2.5 text-sm text-[#8b93a7] font-semibold flex justify-center cursor-pointer": true,
-          "border-b-4 text-orange-500/100 border-b-orange-500/100":
-            activeTab === "transactions",
-        })}
-      >
-        Transactions
-      </div>
+      {tabs.map((tab) => {
+        return (
+          <div
+            onClick={() => dispatch(toggleActiveTab())}
+            className={classnames({
+              "w-1/2 pb-2.5 text-sm text-[#8b93a7] font-semibold flex justify-center cursor-pointer": true,
+              "border-b-4 text-orange-500/100 border-b-orange-500/100":
+                activeTab === tab.value,
+            })}
+          >
+            {tab.name}
+          </div>
+        );
+      })}
     </div>
   );
 }
