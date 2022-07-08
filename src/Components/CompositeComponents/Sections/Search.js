d579d854f66f67884f111a0619463b6e9b211fd3
Sharan <sharanram1993@gmail.com>
2022-07-08 13:04:05 +0530

    Fix search bar

--- src/Components/CompositeComponents/Sections/Search.js
+++ src/Components/CompositeComponents/Sections/Search.js
@@ -1,16 +1,20 @@
-import { useSelector, useDispatch } from "react-redux";
-import { setUserAddress, getUserAddress } from "../../../redux/user";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setUserAddress } from "../../../redux/user";
 
 import Input from "../../AtomicComponents/Input";
 
 function SearchSection() {
-  const userAddress = useSelector(getUserAddress);
+  const [search, setSearch] = useState("");
   const dispatch = useDispatch();
   return (
     <div className="w-5/6 mx-auto py-2">
       <Input
-        value={userAddress}
-        onChange={(e) => dispatch(setUserAddress(e.target.value))}
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+          dispatch(setUserAddress(e.target.value));
+        }}
       />
     </div>
   );
